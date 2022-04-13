29ca5b77ebff5726994903c4c67f8af3048f3798
sooyeon9 <sooyean9@naver.com>
2022-04-13 12:39:45 +0900

    appconfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,13 +12,23 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    // member 정보 저장 방법: 메모리
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // member service 에 필요한 기능: member 조회/저장
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    // order service 에 필요한 기능: member 조회/저장, discount
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-
+    // discount 정책: 고정 할인
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 }
